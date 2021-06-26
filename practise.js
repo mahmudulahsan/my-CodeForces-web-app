be41003efc8b0edae575b909abdcea91d7243d60
mahmudulahsan <59706274+mahmudulahsan@users.noreply.github.com>
2021-06-27 01:38:08 +0600

    problem tags and option added

--- practise.js
+++ practise.js
@@ -1,9 +1,9 @@
 
-
 var press = document.querySelector('#press');
 
 press.addEventListener('click', ()=>{
-    var search = document.querySelector('#text').value;
+    // var search = document.querySelector('#text').value;
+    var search = document.querySelector('.sel').value;
     if(search=="") alert('Please Enter Valid Input');
 
     search = search.toUpperCase();
@@ -16,15 +16,15 @@ press.addEventListener('click', ()=>{
             var table = document.querySelector('#myTable');
 
             var c = 1;
-                   
+            
             for(var i = 0; i<1000; i++){
-                
                 if(arr[i].index==search){
                     var row =`
                     <tr>
                         <td class="bg-dark text-white">${c}</td>
                         <td class="bg-secondary text-white">${arr[i].name}</td>
                         <td class="bg-secondary text-white">${arr[i].rating}</td>
+                        <td class="bg-secondary">${arr[i].tags}</td>
                         <td class="bg-info text-white"><a target="_blank" href="https://codeforces.com/problemset/problem/${arr[i].contestId}/${search}">Link</a></td>
                     </tr>
                     `
@@ -37,3 +37,5 @@ press.addEventListener('click', ()=>{
                buildTable(arr);
     })
 })
+
+
