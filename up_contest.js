3a15dae2acd276156edf51ea16d4b2c0b76d031b
mahmudulahsan <59706274+mahmudulahsan@users.noreply.github.com>
2021-06-29 00:44:43 +0600

    rand

--- up_contest.js
+++ up_contest.js
@@ -13,13 +13,13 @@
             for(var i = 0; i<arr.length; i++){
 
                 if(arr[i].phase=="BEFORE"){
-                    var r_d = arr[i].relativeTimeSeconds/86400;
-                    if(r_d<=0) r_d=r_d*(-1);
+                    var rd = arr[i].relativeTimeSeconds/86400;
+                    if(rd<=0) rd=rd*(-1);
                     var row =`
                     <tr>
                         <td class="bg-dark text-white">${i+1}</td>
                            <td class="bg-secondary text-white">${arr[i].name}</td>
-                           <td class="bg-secondary text-white">${Math.floor(r_d)} days</td>
+                           <td class="bg-secondary text-white">${Math.floor(rd)} days</td>
                            <td class="bg-info text-white"><a target="_blank" href="https://codeforces.com/contests/${arr[i].id}">Contest Link</a></td>
                           </tr>
                       `
