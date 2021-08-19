3ba815157f093085ff7182172eaa9b160d4bf262
mahmudulahsan <59706274+mahmudulahsan@users.noreply.github.com>
2021-08-19 22:31:49 +0600

    contest-sorted

--- up_contest.js
+++ up_contest.js
@@ -6,28 +6,32 @@
      // console.log(data.result.length);
         
      var arr = data.result;
+     var table = document.querySelector('#myTable');
+     var output = [];
   
      function buildTable(arr){
-         var table = document.querySelector('#myTable');
-                
-            for(var i = 0; i<arr.length; i++){
 
+            let cnt = 1;
+        
+            for(var i = 10; i>0; i--){
                 if(arr[i].phase=="BEFORE"){
                     var rd = arr[i].relativeTimeSeconds/86400;
                     if(rd<=0) rd=rd*(-1);
                     var row =`
                     <tr>
-                        <td class="bg-dark text-white">${i+1}</td>
+                        <td class="bg-dark text-white">${cnt}</td>
                            <td class="bg-secondary text-white">${arr[i].name}</td>
                            <td class="bg-secondary text-white">${Math.floor(rd)} days</td>
                            <td class="bg-info text-white"><a target="_blank" href="https://codeforces.com/contests/${arr[i].id}">Contest Link</a></td>
                           </tr>
                       `
-                      table.innerHTML +=row;
+                    //   table.innerHTML +=row;
+                    output.push(row);
+                    cnt++;
                   }
                 }
 
             }
-        
             buildTable(arr);
+            table.innerHTML = output.join(' ');
         })
